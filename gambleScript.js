efae56fb789f3d7b00fc43386a57b9963add8b11
hudson9507 <martinhudson307@gmail.com>
2025-11-16 18:27:58 -0500

    Beginning blackjack

--- gambleScript.js
+++ gambleScript.js
@@ -1,10 +1,13 @@
 //global variable of the current balance
 let currBalance = 0;
+const betButton = document.getElementById('betBtnB');
 
 //Update balance display on page load
 document.addEventListener('DOMContentLoaded', function() {
     updateBalanceDisplay();
 });
+document.addEventListener(onclick, blackjack());
+
 
 //Updates the balance display
 function updateBalanceDisplay() {
@@ -14,4 +17,14 @@ function updateBalanceDisplay() {
     currBalance = balance;
 
     document.getElementById('balanceDisplay').textContent = '$' + currBalance;
+}
+
+function blackjack() {
+    const betInput = document.getElementById('betInputB').value;
+    if (betInput % 1 != 0 ){
+        betInput = Math.floor(betInput);
+    }
+    if (betInput <= 0) {
+        betInput = 1;
+    }
 }
